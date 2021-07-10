044da463e2cd242ea77eae4ad18d038a15b42259
Bhumika1903 <82503987+Bhumika1903@users.noreply.github.com>
2021-07-10 17:25:11 +0530

    Add files via upload

---
+++ addtwonum.java
@@ -0,0 +1,18 @@
+
+// Question :- Given two integers i and j. Write a code to add these two Numbers.
+
+package lecture1;
+
+import java.util.Scanner;
+public class addtwonum {
+	public static void main(String args[]) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+	int i=scan.nextInt();
+	int j=scan.nextInt();
+	
+	int k=i+j;
+	System.out.println(k);
+	}
+}
