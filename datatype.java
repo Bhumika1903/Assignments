5b90b756a1f070d33a0bb30eac4b27d27ee7a234
Bhumika1903 <82503987+Bhumika1903@users.noreply.github.com>
2021-07-12 16:51:24 +0530

    Add files via upload

---
+++ datatype.java
@@ -0,0 +1,25 @@
+//use of datatypes.
+
+
+package lecture1;
+
+public class datatype {
+
+	public static void main(String[] args) {
+		//char c = 'a';
+		//System.out.println(c+1);
+		
+		int i = 'c';
+		System.out.println(i);
+		
+		//char abc = i;
+		long l = i;
+		//i = l;
+		double d = i;
+		//i = d;
+		float f = 1.23f;
+		//System.out.println(f);
+
+	}
+
+}
