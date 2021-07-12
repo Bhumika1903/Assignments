d91ef6790ef7501fd29df68bdc400f728fe1d5bd
Bhumika1903 <82503987+Bhumika1903@users.noreply.github.com>
2021-07-12 16:56:11 +0530

    Add files via upload

---
+++ evenodd.java
@@ -0,0 +1,27 @@
+//Question :- Given a number n check whether the number is even or odd return ans in true or false. 
+
+package if_else;
+import java.util.Scanner;
+
+public class evenodd 
+{
+
+	public static void main(String[] args)
+	{
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int rem = n % 2;
+		if (rem == 0) 
+		{
+			System.out.println("Even");
+			return;
+		} 
+		
+		
+		System.out.println("Odd"); 
+		
+
+	}
+
+}
+
