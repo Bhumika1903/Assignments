f05201f919fd189485a4b1128fcd5b5c1cb1ba91
Bhumika1903 <82503987+Bhumika1903@users.noreply.github.com>
2021-07-12 16:53:01 +0530

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,11 @@
+// Question :- print HELLO WORLD.
+
+package lecture1;
+
+public class helloworld {
+   public static void main(String args[]){
+	   System.out.println("HelloWorld");
+	   System.out.print("HelloWorld");
+	   System.out.println("HelloWorld");
+   }
+}
